c7f7066a8d7c9b63a54d4f9f4978bb6898a3982d
Sunandasarilla <101045781+Sunandasarilla@users.noreply.github.com>
2026-08-17 22:28:23 +0530

    Revise script execution instructions in syntax.pl
    Updated script usage instructions and examples in syntax.pl.

--- syntax.pl
+++ syntax.pl
@@ -1,11 +1,19 @@
 #files should be in .pl or .pm format
-print "files should be in .pl or .pm format\n";
+#Example 
+scripting.pl
 
 #how to run the script 
-print "perl <filename> to run the script\n";
-print "./<scriptname>   for this permissions should be 755 then only it will execute\n";
+#method1:
+perl <filename> to run the script
+perl scripting.pl 
+#method 2: 
+./<scriptname>   for this permissions should be 755 then only it will execute
+./scripting.pl
+
 #to run the script in interactive mode
-print "perl -e somecode to run in interactive mode\n";
+perl -e somecode to run in interactive mode
+perl -e print "hi\n";
+
 #each line should end with a semicolon
 print "each line should end with ;\n";
 
