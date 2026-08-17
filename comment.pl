0b84ab7911022f2e734ce697e6cc81e41fe860c5
Sunandasarilla <101045781+Sunandasarilla@users.noreply.github.com>
2026-08-17 22:45:46 +0530

    Add files via upload

---
+++ comment.pl
@@ -0,0 +1,16 @@
+#this program illustrates commenting in perl scripting
+
+#one line commenting
+
+=somename
+this is how we crepresent multiple line commenting
+by ending it with
+=cut
+
+<<somename
+ this is how we represent multiple line commenting
+ by using lessthan symbol followed by some name 
+ then ending it with the same name
+somename
+
+
