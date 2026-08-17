45d1e5b96fceeb7a6200eaf1a1295d171ff34228
Sunandasarilla <101045781+Sunandasarilla@users.noreply.github.com>
2026-08-17 22:19:22 +0530

    Add files via upload

---
+++ syntax.pl
@@ -0,0 +1,11 @@
+#files should be in .pl or .pm format
+print "files should be in .pl or .pm format\n";
+
+#how to run the script 
+print "perl <filename> to run the script\n";
+print "./<scriptname>   for this permissions should be 755 then only it will execute\n";
+#to run the script in interactive mode
+print "perl -e somecode to run in interactive mode\n";
+#each line should end with a semicolon
+print "each line should end with ;\n";
+
